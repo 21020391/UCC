b4d1036a2e999f9f35d05541bf3046ea6a5eac6b
xinfan03 <21020391@vnu.edu.vn>
2023-11-29 19:50:37 +0700

    v2

--- client/test/elements/newNote.spec.js
+++ client/test/elements/newNote.spec.js
@@ -1,40 +1,137 @@
-const { By, Builder, until, Select } = require("selenium-webdriver");
-const assert = require('assert');
+const { suite } = require("selenium-webdriver/testing");
+const { By, Builder, until, Select, Browser } = require("selenium-webdriver");
+const assert = require("assert");
 
-describe("New Note Test", function () {
-  let driver;
+suite(
+    function (env) {
+        describe("New Note Test", function () {
+            let driver;
 
-  before(async function () {
-    driver = await new Builder().forBrowser("chrome").build();
-  });
+            before(async function () {
+                driver = await env.builder().build();
+            });
 
-  it("Dang nhap", async function () {
-    //const input = {[title,description]:[1,1],[a,k],[1,q],[a,2]};
+            after(() => driver.quit());
 
-    await driver.manage().setTimeouts({ implicit: 5000});
+            // Test 1
+            it("Create Note Test 1", async function () {
 
-    // Navigate to URL
-    await driver.get("http://localhost:5173/login");
-    await driver.findElement(By.id("username")).sendKeys("aa");
-    await driver.findElement(By.id("password")).sendKeys("11");
-    await driver
-      .findElement(By.className("btn btn-primary btn-block confirm-button"))
-      .click();
-      await driver.findElement(By.id("newNote")).click();
-      await driver.findElement(By.id("title")).sendKeys("aa");
-      await driver.findElement(By.id("description")).sendKeys("11");
-      await driver.findElement(By.id("status")).click();
+                await driver.manage().setTimeouts({ implicit: 5000 });
 
-      const zeroElement = await driver.findElement(By.css("option[value='0']"))
-      const oneElement = await driver.findElement(By.css("option[value='1']"))
-      const twoElement = await driver.findElement(By.css("option[value='2']"))
+                // Navigate to URL
+                await driver.get("http://localhost:5173/login");
+                await driver.findElement(By.id("username")).sendKeys("aa");
+                await driver.findElement(By.id("password")).sendKeys("11");
+                await driver
+                    .findElement(By.className("btn btn-primary btn-block confirm-button"))
+                    .click();
 
-      zeroElement.click();
-      await driver
-      .findElement(By.className("btn btn-primary btn-block confirm-button"))
-      .click();
-    
+                await driver.findElement(By.id("newNote")).click();
+                await driver.findElement(By.id("title")).sendKeys("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+                await driver.findElement(By.id("description")).sendKeys("1111111111111111111111111111111111111111111111111111111111");
+                await driver.findElement(By.id("status")).click();
 
-    //after(() => driver.quit());
-  });
-});
+                await driver.findElement(By.css("option[value='0']")).click();
+
+                await driver
+                    .findElement(By.className("btn btn-primary btn-block confirm-button"))
+                    .submit();
+
+                // Wait for the page navigation back to the Notes page
+                await driver.wait(until.urlIs('http://localhost:5173/notes'), 5000);
+
+                const currentUrl = await driver.getCurrentUrl();
+                assert.equal(currentUrl, 'http://localhost:5173/notes');
+            });
+
+            // Test 2
+            it("Create Note Test 2", async function () {
+
+                await driver.manage().setTimeouts({ implicit: 5000 });
+
+                // Navigate to URL
+                await driver.get("http://localhost:5173/login");
+                await driver.findElement(By.id("username")).sendKeys("aa");
+                await driver.findElement(By.id("password")).sendKeys("11");
+                await driver
+                    .findElement(By.className("btn btn-primary btn-block confirm-button"))
+                    .click();
+
+                await driver.findElement(By.id("newNote")).click();
+                await driver.findElement(By.id("title")).sendKeys("                                                        ");
+                await driver.findElement(By.id("description")).sendKeys("1111111111111111111111111111111111111111111111111111111111");
+                await driver.findElement(By.id("status")).click();
+
+                await driver.findElement(By.css("option[value='1']")).click();
+                await driver
+                    .findElement(By.className("btn btn-primary btn-block confirm-button"))
+                    .click();
+
+                // Wait for the page navigation back to the Notes page
+                await driver.wait(until.urlIs('http://localhost:5173/notes'), 5000);
+
+                const currentUrl = await driver.getCurrentUrl();
+                assert.equal(currentUrl, 'http://localhost:5173/notes');
+            });
+
+            // Test 3
+            it("Create Note Test 3", async function () {
+
+                await driver.manage().setTimeouts({ implicit: 5000 });
+
+                // Navigate to URL
+                await driver.get("http://localhost:5173/login");
+                await driver.findElement(By.id("username")).sendKeys("aa");
+                await driver.findElement(By.id("password")).sendKeys("11");
+                await driver
+                    .findElement(By.className("btn btn-primary btn-block confirm-button"))
+                    .click();
+
+                await driver.findElement(By.id("newNote")).click();
+                await driver.findElement(By.id("title")).sendKeys("1");
+                await driver.findElement(By.id("status")).click();
+
+                await driver.findElement(By.css("option[value='2']")).click();
+                await driver
+                    .findElement(By.className("btn btn-primary btn-block confirm-button"))
+                    .click();
+
+                // Wait for the page navigation back to the Notes page
+                await driver.wait(until.urlIs('http://localhost:5173/notes'), 5000);
+
+                const currentUrl = await driver.getCurrentUrl();
+                assert.equal(currentUrl, 'http://localhost:5173/notes');
+            });
+
+            // Test 4
+            it("Create Note Test 4", async function () {
+
+                await driver.manage().setTimeouts({ implicit: 5000 });
+
+                // Navigate to URL
+                await driver.get("http://localhost:5173/login");
+                await driver.findElement(By.id("username")).sendKeys("aa");
+                await driver.findElement(By.id("password")).sendKeys("11");
+                await driver
+                    .findElement(By.className("btn btn-primary btn-block confirm-button"))
+                    .click();
+
+                await driver.findElement(By.id("newNote")).click();
+                await driver.findElement(By.id("description")).sendKeys("1111111111111111111111111111111111111111111111111111111111");
+                await driver.findElement(By.id("status")).click();
+
+                await driver.findElement(By.css("option[value='0']")).click();
+                await driver
+                    .findElement(By.className("btn btn-primary btn-block confirm-button"))
+                    .click();
+
+                // Wait for the page navigation back to the Notes page
+                await driver.wait(until.urlIs('http://localhost:5173/notes'), 5000);
+
+                const currentUrl = await driver.getCurrentUrl();
+                assert.equal(currentUrl, 'http://localhost:5173/notes/create');
+            });
+        });
+    },
+    { browsers: [Browser.CHROME, Browser.EDGE] }
+);
